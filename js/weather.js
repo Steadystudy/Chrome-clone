234684e1aec9a92b6d88a65610e1a05188fee540
Samuel <sgmin206@naver.com>
2022-01-09 23:41:12 +0900

    css 조정중

--- js/weather.js
+++ js/weather.js
@@ -4,12 +4,12 @@ const geoOk = (position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(lat, lon);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector(".weather span:first-child");
       const city = document.querySelector(".weather span:last-child");
-      console.log(city);
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main}`;
     });
